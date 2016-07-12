b1be37b2af341f005b9f2948d8435230cf6282af
nsonanh <n.sonanh@yahoo.com>
2016-07-12 22:37:30 +0700

    Initial commit

---
+++ simple-reminder/imports/ui/body.js
@@ -0,0 +1,29 @@
+<body>
+  <div class="container">
+    <header>
+      <h1>Todo List</h1>
+    </header>
+ 
+    <ul>
+      {{#each tasks}}
+        {{> task}}
+      {{/each}}
+    </ul>
+  </div>
+</body>
+ 
+<template name="task">
+  <li>{{text}}</li>
+</template>
+
+import { Template } from 'meteor/templating';
+ 
+import './body.html';
+ 
+Template.body.helpers({
+  tasks: [
+    { text: 'This is task 1' },
+    { text: 'This is task 2' },
+    { text: 'This is task 3' },
+  ],
+});
